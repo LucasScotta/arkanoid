5d32594251e11fd969d9d812cbccba692d7541c1
Lucas <lucasmanuelscotta@gmail.com>
2020-06-02 19:21:44 -0300

    cambio en containerRect

--- crear-blocks.js
+++ crear-blocks.js
@@ -1,9 +1,8 @@
 function blocks(level) {
 
 	const divBlock = []
-	const rect = container.getBoundingClientRect()
-	let contX = rect.left + 20
-	let contY = rect.top + 15
+	let contX = containerRect.left + 20
+	let contY = containerRect.top + 15
 	let divN = 2
 
 	for (let i = -1; i < level; i += 1) {
@@ -15,7 +14,7 @@ function blocks(level) {
 			divBlock[i][j] = document.createElement('div')
 			container.appendChild(divBlock[i][j])
 
-			divBlock[i][j].right = rect.right
+			divBlock[i][j].right = containerRect.right
 			divBlock[i][j].classList.add('box')
 			container.children[divN].style.left = contX + 'px'
 			container.children[divN].style.top = contY + 'px'
@@ -24,7 +23,7 @@ function blocks(level) {
 
 			if (j === 6) {
 				contY += 55
-				contX -= rect.width
+				contX -= containerRect.width
 			}
 		}
 	}
