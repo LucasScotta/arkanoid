7c746de77054bec23b5678b2e9fb7b40e29e4336
Lucas <lucasmanuelscotta@gmail.com>
2020-06-04 12:05:17 -0300

    documentacion del objeto nave agregada en nave.js

--- nave.js
+++ nave.js
@@ -3,17 +3,29 @@ const nave = {
 		w: 100,
 		h: 15,
 	},
+	/**
+	 * Alarga la nave al agarrar el powerUp 'alargar'
+	 */
 	alargar: function () {
 
 		return this.size.w += 50
 	},
+	/**
+	 * Achica la nave al agarrar el powerUp 'achicar'
+	 */
 	achicar: function () {
 
 		return this.size.w -= 50
 	},
+	/**
+	 * Mueve la nave al mover el mouse
+	 */
 	mover: function () {
 
 	},
+	/**
+	 * Reinicia los powerUps de la nave
+	 */
 	reiniciar: function () {
 
 		return this.size.w = 100
