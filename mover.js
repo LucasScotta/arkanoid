495b8c962ff0fd1413d099a7f3b33f4bc2863c6a
Lucas <lucasmanuelscotta@gmail.com>
2020-06-10 22:41:17 -0300

    cheats agregados

--- mover.js
+++ mover.js
@@ -27,20 +27,37 @@ document.onclick = function(event) {
 }
 
 document.onkeydown = function tecla(event) {
-
-		if (event.key === 'p') {
-
+	if (event.key === 'p') {
 		game.config.pause = !game.config.pause
+	}
+	if (event.code === 'Space') {
+		for (let ball of balls) {
+		
+			ball.arrancar(mouse.x, mouse.y, game, nave)
+		}
+	}
+	if (event.key === 'l') {
+			randomOf(balls).agregarBall()
+	}
+	if (event.key === 'm') {
+		nave.setWidthType(widthTypes.L)
+	}
+	if (event.key === 'n') {
+		nave.setWidthType(widthTypes.S)
+	}
+	if (event.key === 'b') {
+		nave.setWidthType(widthTypes.M)
+	}
+	if (event.key === 'v') {
+		nave.activarGun()
 	}
 }
 
 function pintarBolaInicio() {
 
-	const ball = balls[0]
-	const $ball = $balls[0]
-	ball.pos.x = nave.pos.x + nave.size.w / 2
-	ball.pos.y = nave.pos.y - ball.size.h - 1
+	balls[0].pos.x = nave.pos.x + nave.size.w / 2
+	balls[0].pos.y = nave.pos.y - balls[0].size.h - 1
 
-	$ball.style.left = `${ball.pos.x}px`
-	$ball.style.top  = `${ball.pos.y}px`
+	balls[0].$el.style.left = `${balls[0].pos.x}px`
+	balls[0].$el.style.top  = `${balls[0].pos.y}px`
 }
