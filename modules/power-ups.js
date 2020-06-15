49ce7744ead2538ae24a8d3d92cb2a029b6a96ab
Lucas <lucasmanuelscotta@gmail.com>
2020-06-15 10:26:05 -0300

    imgs agregadas al head

---
+++ modules/power-ups.js
@@ -0,0 +1,11 @@
+/*globals define*/
+define(['proto/powerUps.js'], function (bloques) {
+[
+	bloques.normal,
+	bloques.multiplicador,
+	bloques.goma,
+	bloques.disparo,
+	bloques.agrandador,
+	bloques.achichador,
+]
+})
