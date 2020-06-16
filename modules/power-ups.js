1aec2215951683376bf7424c721b2fbd6b051b60
Lucas <lucasmanuelscotta@gmail.com>
2020-06-16 00:55:25 -0300

    modules/power-ups arreglado

--- modules/power-ups.js
+++ modules/power-ups.js
@@ -1,11 +1,11 @@
 /*globals define*/
-define(['proto/powerUps.js'], function (bloques) {
-[
-	bloques.normal,
-	bloques.multiplicador,
-	bloques.goma,
-	bloques.disparo,
-	bloques.agrandador,
-	bloques.achichador,
-]
+define(['proto/powerUps'], function (bloques) {
+	return [
+		bloques.normal,
+		bloques.multiplicador,
+		bloques.goma,
+		bloques.disparo,
+		bloques.agrandador,
+		bloques.achichador,
+	]
 })
