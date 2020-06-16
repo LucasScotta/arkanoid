d12484c8fb6edce072edf00ec51a2b07b34137ad
Lucas <lucasmanuelscotta@gmail.com>
2020-06-16 01:16:08 -0300

    modules/new-ball

---
+++ modules/new-ball.js
@@ -0,0 +1,32 @@
+/*globals define*/
+define(['modules/balls',
+		'proto/ball'], function(balls, Ball) {
+	const imgBalls = 'img/ball.png'
+	const newBallDiv = document.createElement('div')
+	const newBallImg = document.createElement('img')
+	newBallDiv.appendChild(newBallImg)
+	newBallImg.classList.add('ball')
+	newBallImg.src = imgBalls
+	balls.push(new Ball({
+	$el: newBallDiv,
+	$img: newBallImg,
+	pos: {
+		x: 0,
+		y: 0,
+	},
+	config: {
+		ballDirX: 0,
+		ballDirY: 0,
+	},
+	vel: {
+		r: 5,
+		a: Math.PI,
+	},
+	size : {
+		w: 15,
+		h: 15,
+	},
+	goma: false,
+	}))
+	document.getElementById('container').appendChild(newBallDiv)
+})
