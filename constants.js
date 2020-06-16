1a4b84b7fa8ee6d450592a0ec1955ca95737886d
Lucas <lucasmanuelscotta@gmail.com>
2020-06-16 19:23:22 -0300

    constantes declaradas en un documento nuevo

---
+++ constants.js
@@ -0,0 +1,24 @@
+/*globals define*/
+define(['modules/potenciadores',
+		'modules/nave',], (bloques, nave) => {
+	return {
+		imgsBox: [
+		'img/0.png',
+		'img/1.png',
+		'img/2.png',
+		'img/3.png',
+		'img/4.png',
+		'img/5.png',
+		'img/6.png',
+		'img/7.png',
+		],
+		power: [
+		bloques.multiplicador,
+		bloques.goma,
+		bloques.disparo,
+		bloques.agrandador,
+		bloques.achichador,
+		],
+		naveInicial: parseInt(nave.pos.x)
+	}
+})
