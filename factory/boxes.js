b04d24390d3cee72d17bc1eaeeb0817e6bb5829f
Lucas <lucasmanuelscotta@gmail.com>
2020-06-16 21:31:08 -0300

    factory/boxes

---
+++ factory/boxes.js
@@ -0,0 +1,31 @@
+'use strict'
+/*globals define*/
+define(['globals',
+		'constants',
+		'modules/randomOf',
+		'proto/box',], (globals, constants, randomOf, Box) => {
+			let img
+	const createElement = () => {
+		const $el = document.createElement('div')
+		img = document.createElement('img')
+		$el.appendChild(img)
+		$el.classList.add('box')
+		img.classList.add('box')
+		document.getElementById('container').appendChild($el)
+		return $el
+	}
+	return (posX, posY) => new Box({
+		$div: createElement(),
+		$img: img,
+		pos: {
+			x: posX,
+			y: posY,
+		},
+		size: {
+			w: 60,
+			h: 20,
+		},
+		strong: Math.floor(Math.random() * constants.imgsBox.length),
+		power: randomOf(constants.power),
+	})
+})
