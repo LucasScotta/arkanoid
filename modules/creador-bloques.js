e756c1f6b314f18024f77f3f46518b3541868861
Lucas <lucasmanuelscotta@gmail.com>
2020-06-16 21:33:26 -0300

    nueva manera de crear las cajas

--- modules/creador-bloques.js
+++ modules/creador-bloques.js
@@ -3,14 +3,12 @@
 define(['globals',
 		'constants',
 		'modules/game',
-		'proto/box',
-		'modules/randomOf',
+		'factory/boxes'
 ], (
 	globals,
 	constants,
 	game,
-	Box,
-	randomOf,) => {
+	newBox,) => {
 	let k
 	k = 0
 	let posX = game.pos.x + game.size.b * 2
@@ -19,28 +17,8 @@ define(['globals',
 	for (let i = 0; i < game.config.level + 3; i += 1) {
 		
 		for (let j = 0; j <= 8; j += 1) {
-			const div = document.createElement('div')
-			const img = document.createElement('img')
-			div.appendChild(img)
-			div.classList.add('box')
-			img.classList.add('box')
-			globals.boxes[k] = new Box({
-				pos: {
-					x: posX,
-					y: posY,
-				},
-				size: {
-					w: 60,
-					h: 20,
-				},
-				strong: Math.floor(Math.random() * constants.imgsBox.length),
-				$div: div,
-				$img: img,
-				index: k,
-				power: randomOf(constants.power),
-			})
+			globals.boxes[k] = newBox(posX, posY)
 			globals.boxes[k].$img.src = constants.imgsBox[globals.boxes[k].strong]
-			document.getElementById('container').appendChild(div)
 			globals.boxes[k].$div.style.left = posX + 'px'
 			globals.boxes[k].$div.style.top = posY + 'px'
 			posX += 75
