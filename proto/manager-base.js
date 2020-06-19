6e66ad359dd1b8e9f0f878c11e4505d98fa0cce7
Lucas <lucasmanuelscotta@gmail.com>
2020-06-19 18:20:25 -0300

    manager-base

---
+++ proto/manager-base.js
@@ -0,0 +1,32 @@
+/*globals define*/
+define(() => {
+
+	return class ManagerBase {
+		constructor() {
+			this.items = []
+		}
+		agregar(item) {
+			this.items.push(item)
+		}
+		remover(item) {
+			const index = this.items.indexOf(item)
+			const existe = index >= 0
+			if (existe) {
+				this.items.splice(index, 1)
+			}
+			return existe
+		}
+		estaVacio() {
+			return this.items.length === 0
+		}
+		getItems() {
+			return this.items
+		}
+		getItem(index) {
+			return this.items[index]
+		}
+		reset() {
+			this.items.length = 0
+		}
+	}
+})
