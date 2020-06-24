3119a10333d5f76be4e1eeb198068173b4a9e9a8
Lucas <lucasmanuelscotta@gmail.com>
2020-06-24 12:25:42 -0300

    update movido al index

--- index.js
+++ index.js
@@ -11,7 +11,38 @@ define('img/box', () => [
 	'img/7.png',
 	])
 
-require(['proto/game'], function (Game) {
-	const game = new Game()
+require(['proto/game',
+		'globals',], function (Game, globals) {
+	window.game = new Game()
 	game.start()
+	function update() {
+		
+		for (const ball of game.ballm.getBalls()) {
+			ball.moverInicio(game, globals)
+			ball.update(game)
+		}
+		
+		if (globals.mouse.b) {
+			game.nave.mover(globals.mouse.x, globals.mouse.y)
+			globals.mouse.b = false
+		}
+
+
+		for (const power of game.powerm.getItems()) {
+			power.update()
+		}
+
+		if (game.boxm.estaVacio()) {
+			return game.ganar(globals)
+		}
+		game.nave.gun.update(globals.game)
+	}
+
+	function loop() {
+
+		if (!game.config.pause) update()
+	}
+
+	let time = 5
+	setInterval(loop, time)
 })
