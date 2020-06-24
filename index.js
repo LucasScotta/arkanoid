05ca0549b40aab37b5e82e08215d16ed67f8f321
Lucas <lucasmanuelscotta@gmail.com>
2020-06-24 20:41:58 -0300

    extends commit anterior

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 'use strict'
-/* globals require, define*/
-define('img/box', () => [
-	'img/0.png',
-	'img/1.png',
-	'img/2.png',
-	'img/3.png',
-	'img/4.png',
-	'img/5.png',
-	'img/6.png',
-	'img/7.png',
-	])
-
+/* globals require*/
 require(['proto/game',
 		'globals',
 		'mover',], function (Game, globals, coord) {
